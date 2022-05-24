9cd7533d48f4f05f60bc38eb2f7a89f80980366b
nopnop2002 <hau26280@tree.odn.ne.jp>
2022-05-24 17:41:50 +0900

    Changed from tab to space

--- main/RCSwitch.c
+++ main/RCSwitch.c
@@ -42,18 +42,18 @@
 #define TAG "RF433"
 
 static const Protocol proto[] = {
-	{ 350, {	1, 31 }, {	1,	3 }, {	3,	1 }, false },	// protocol 1
-	{ 650, {	1, 10 }, {	1,	2 }, {	2,	1 }, false },	// protocol 2
-	{ 100, { 30, 71 }, {	4, 11 }, {	9,	6 }, false },	// protocol 3
-	{ 380, {	1,	6 }, {	1,	3 }, {	3,	1 }, false },	// protocol 4
-	{ 500, {	6, 14 }, {	1,	2 }, {	2,	1 }, false },	// protocol 5
-	{ 450, { 23,	1 }, {	1,	2 }, {	2,	1 }, true },	// protocol 6 (HT6P20B)
-	{ 150, {	2, 62 }, {	1,	6 }, {	6,	1 }, false },	// protocol 7 (HS2303-PT, i. e. used in AUKEY Remote)
-	{ 200, {	3, 130}, {	7, 16 }, {	3,	16}, false},	// protocol 8 Conrad RS-200 RX
-	{ 200, { 130, 7 }, {	16, 7 }, { 16,	3 }, true},		// protocol 9 Conrad RS-200 TX
-	{ 365, { 18,	1 }, {	3,	1 }, {	1,	3 }, true },	// protocol 10 (1ByOne Doorbell)
-	{ 270, { 36,	1 }, {	1,	2 }, {	2,	1 }, true },	// protocol 11 (HT12E)
-	{ 320, { 36,	1 }, {	1,	2 }, {	2,	1 }, true }		// protocol 12 (SM5212)
+  { 350, {  1, 31 }, {  1,  3 }, {  3,  1 }, false },    // protocol 1
+  { 650, {  1, 10 }, {  1,  2 }, {  2,  1 }, false },    // protocol 2
+  { 100, { 30, 71 }, {  4, 11 }, {  9,  6 }, false },    // protocol 3
+  { 380, {  1,  6 }, {  1,  3 }, {  3,  1 }, false },    // protocol 4
+  { 500, {  6, 14 }, {  1,  2 }, {  2,  1 }, false },    // protocol 5
+  { 450, { 23,  1 }, {  1,  2 }, {  2,  1 }, true },     // protocol 6 (HT6P20B)
+  { 150, {  2, 62 }, {  1,  6 }, {  6,  1 }, false },    // protocol 7 (HS2303-PT, i. e. used in AUKEY Remote)
+  { 200, {  3, 130}, {  7, 16 }, {  3,  16}, false},     // protocol 8 Conrad RS-200 RX
+  { 200, { 130, 7 }, {  16, 7 }, { 16,  3 }, true},      // protocol 9 Conrad RS-200 TX
+  { 365, { 18,  1 }, {  3,  1 }, {  1,  3 }, true },     // protocol 10 (1ByOne Doorbell)
+  { 270, { 36,  1 }, {  1,  2 }, {  2,  1 }, true },     // protocol 11 (HT12E)
+  { 320, { 36,  1 }, {  1,  2 }, {  2,  1 }, true }      // protocol 12 (SM5212)
 };
 
 enum {
